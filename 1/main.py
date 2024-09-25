dae3c6fd9d382777198d28bcd1dc0a23f73f07e3
kagacewho <whykpsad@tutamail.com>
2024-09-25 10:15:25 +0300

    Add files via upload

---
+++ 1/main.py
@@ -0,0 +1,11 @@
+input_data = open('input.txt', 'r') 
+data = input_data.read()
+from decimal import Decimal
+E = Decimal("2.7182818284590452353602875")
+input_data = open('input.txt', 'r')
+round_data = int(input_data.read())
+out = round(E, round_data)
+output_data = open('output.txt', 'w')
+output_data.write(str(out))
+input_data.close()
+output_data.close()
